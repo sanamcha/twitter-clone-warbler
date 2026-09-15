3c4f301e9c87291e5429f6216fa00cb2602b1724
Sanam Maharjan <sanamcha@Sanams-MacBook-Air-2.local>
2026-09-14 19:25:25 -0700

    Fix Render PostgreSQL deployment

--- app.py
+++ app.py
@@ -14,8 +14,11 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+database_url = os.environ.get('DATABASE_URL', 'postgresql:///warbler')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://', 'postgresql://', 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
